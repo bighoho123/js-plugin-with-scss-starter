2a151fd0121d07fa9c7454c1cea54dd485f29d66
Jinzhe <jinzhe@simple.com.au>
2018-05-24 16:26:19 +0930

    Update to Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -26,7 +25,6 @@ module.exports = {
         ]
     },
     plugins: [
-        new uglifyJsPlugin(),
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'index.html')
         }),
